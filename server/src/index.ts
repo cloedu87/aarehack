f71d81a76762d95a068d51adfa41bafaef29256a
claudio <cloedu@gmail.com>
2019-06-18 18:33:21 +0200

    returns correct data. but super ugly. refactoring and fe works ahead.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,26 @@
 import * as express from 'express';
 import { Request, Response } from 'express';
+import * as request from 'request';
+
 const app = express();
 const {
     PORT = 3000,
 } = process.env;
 app.get('/', (req: Request, res: Response) => {
-    res.send({
-        message: 'hello sdfsdfsdf',
+
+    request.get('https://aareguru.existenz.ch/v2018/current?city=bern', {timeout: 1000}, function (error, response, body) {
+        if(response.statusCode == 200){
+
+            let aareJson = JSON.parse(body);
+            res.send(JSON.stringify(aareJson.aare.temperature));
+
+        } else {
+            console.log('error: '+ response.statusCode)
+            console.log(body)
+        }
     });
+
+
 });
 app.listen(PORT, () => {
     console.log('server started at http://localhost:'+PORT);
