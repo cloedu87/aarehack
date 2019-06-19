1c881b00e3223cf7a138ee7a0ba9f325e636f5eb
claudio <cloedu@gmail.com>
2019-06-20 00:21:13 +0200

    added server sent event handling on backend side, added some typescript structure

---
+++ server/src/App.ts
@@ -0,0 +1,74 @@
+import * as express from 'express'
+import {Request} from "express";
+import {Response} from "express";
+import * as request from 'request';
+import * as dayjs from 'dayjs';
+import {Emitter} from './sseEmitter';
+
+class App {
+    public express;
+
+    constructor() {
+        this.express = express();
+        this.mountRoutes();
+        this.listen(3000);
+        this.listen(5001);
+    }
+
+    private mountRoutes(): void {
+        const router = express.Router();
+        const events = new Emitter();
+
+        this.express.use('/', router);
+        this.express.use('/events', router);
+        this.express.use(express.static('public'));
+
+        this.express.locals.dayjs = dayjs;
+        this.express.set('view engine', 'pug');
+
+        router.get('/', (req: Request, res: Response) => {
+
+            this.grabit(function (aareJson) {
+
+                res.render('index', {
+                    title: 'aare hack',
+                    temperature: JSON.stringify(aareJson.aare.temperature) + ' °C',
+                    flow: JSON.stringify(aareJson.aare.flow) + ' m3/s',
+                    history: aareJson.aarepast
+                })
+            })
+        });
+
+        router.get('/events', events.subscribe);
+
+    }
+
+    private listen(port: number): void {
+
+        this.express.listen(port, (err) => {
+            if (err) {
+                return console.log(err)
+            }
+
+            return console.log(`server is listening on ${port}`)
+        });
+    }
+
+    private grabit(callback: CallableFunction): void {
+
+        request.get('https://aareguru.existenz.ch/v2018/current?city=bern', {timeout: 1000}, function (error, response, body) {
+            if (response.statusCode == 200) {
+
+                let aareJson = JSON.parse(body);
+
+                callback(aareJson);
+
+            } else {
+                console.log('error: ' + response.statusCode);
+                console.log(body)
+            }
+        });
+    }
+}
+
+export default new App().express
---
+++ server/src/sseEmitter.ts
@@ -0,0 +1,54 @@
+import * as EventEmitter from 'eventemitter3';
+import {Request, Response} from "express";
+
+export class Emitter {
+
+    public emitter;
+
+    constructor() {
+        this.emitter = new EventEmitter();
+    }
+
+    public subscribe = (req: Request, res: Response) => {
+        res.writeHead(200, {
+            'Content-Type': 'text/event-stream',
+            'Cache-Control': 'no-cache',
+            Connection: 'keep-alive'
+        });
+
+        //send constant signal over http connection
+        const heartbeat = this.heart(() => {
+            res.write('\n');
+        });
+
+        //if event getts triggered, send new data to client
+        const onEvent = function (data) {
+            res.write('retry: 500\n');
+            res.write(`event: event\n`);
+            res.write(`data: ${JSON.stringify(data)}\n\n`);
+        };
+
+        this.emitter.on('event', onEvent);
+
+        req.on('close', function () {
+
+            //kill constant signal to client
+            clearInterval(heartbeat);
+
+            //kill http connection
+            this.emitter.removeListener('event', onEvent);
+        });
+    };
+
+    public publish = (eventData: any) => {
+
+        this.emitter.emit('event', eventData);
+    };
+
+    private heart(beat: CallableFunction): number {
+
+        return setInterval(beat, 10000);
+    }
+}
+
+export default new Emitter().emitter;
